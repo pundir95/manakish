e3abd9e79f97bfe716964bef37ab2ad630d795f4
pankaj <pankaj@avioxtechnologies.com>
2024-11-12 14:54:46 +0530

    worked on new

--- src/app/(home)/home/page.jsx
+++ src/app/(home)/home/page.jsx
@@ -1,9 +1,6 @@
 import ContactSection from "@/_components/ContactSection";
 import Footer from "@/_components/Footer";
 import HeaderSection from "@/_components/HeaderSection";
-import HomePageBanner from "@/_components/HomePageBanner";
-import HomePageHeader from "@/_components/HomePageHeader";
-import HomePageSectionTwo from "@/_components/HomePageSectionTwo";
 import LimitedTimeOffers from "@/_components/LimitedTimeOffers";
 import Navbar from "@/_components/Navbar";
 import OurStorySection from "@/_components/OurStorySection";
@@ -13,16 +10,11 @@ import SignatureDelightBanner from "@/_components/SignatureDelightBanner";
 export default function Home() {
   return (
     <div className="home-page">
-      {/* <HomePageHeader /> */}
-      {/* <HomePageSectionTwo /> */}
       <HeaderSection />
       <OurStorySection />
       <ProductsSection />
       <SignatureDelightBanner />
       <LimitedTimeOffers />
-      {/* <HomePageBanner />
-      <ContactSection />
-      <Footer /> */}
     </div>
   );
 }
