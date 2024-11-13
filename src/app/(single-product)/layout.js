8b1f59c5869b116e1ae5687db8e202a3b165b032
pankaj <pankaj@avioxtechnologies.com>
2024-11-13 16:27:10 +0530

    Worked on single product layout

---
+++ src/app/(single-product)/layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ContactSection from "@/_components/ContactSection";
+import Footer from "@/_components/Footer";
+import HomePageBanner from "@/_components/HomePageBanner";
+import Navbar from "@/_components/Navbar";
+
+const SingleProduct = ({ children }) => {
+  return (
+    <div>
+      {" "}
+      {/* <Navbar /> */}
+      {children}
+      {/* <HomePageBanner />
+      <ContactSection />
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default SingleProduct;
---
+++ src/app/(single-product)/single-product/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const SingleProduct = () => {
+  return (
+    <div>SingleProduct</div>
+  )
+}
+
+export default SingleProduct
