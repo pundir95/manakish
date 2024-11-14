440ad3c669ecbd28359b8a8ddb3842f98ec2335d
Pankaj <Pankaj@avioxtechnologies.com>
2024-11-14 16:33:29 +0530

    implemented autocomplete

--- src/app/(home)/home/page.jsx
+++ src/app/(home)/home/page.jsx
@@ -15,6 +15,7 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BiEdit } from "react-icons/bi";
 import CategoryCarousel from "@/_components/CategoryCarousel";
 import ProductsSection from "@/_components/ProductsSection";
+import Autocomplete from "react-google-autocomplete";
 
 export default function Home() {
   const [selectedOption, setSelectedOption] = useState("Delivery");
@@ -43,35 +44,65 @@ export default function Home() {
               }`}
               onClick={() => setSelectedOption("Pickup")}
             >
-              Pickup
+              Pickup{" "}
             </button>
           </div>
 
           {/* Delivery Information */}
-          <div className="mb-4">
-            <label className="block text-gray-600 mb-1">Delivery To</label>
-            <div className="relative">
-              <select className="w-full border border-orange-200 rounded-full p-2 text-gray-800 bg-[#faf5ed]">
+          {selectedOption === "Delivery" ? (
+            <>
+              <div className="mb-2">
+                <label className="block text-gray-600 mb-1">Delivery To</label>
+                <div className="relative">
+                  {/* <select className="w-full border border-orange-200 rounded-full p-2 text-gray-800 bg-[#faf5ed]">
                 <option>Abdullah al Mubarak (West Jeleeb) Edit</option>
-              </select>
-            </div>
-          </div>
+              </select> */}
+                  <Autocomplete
+                    apiKey={process.env.NEXT_PUBLIC_GOOGLE_AUTOCOMPLETE_API_KEY}
+                    onPlaceSelected={(place) => {
+                      const lat = place.geometry.location.lat();
+                      const lng = place.geometry.location.lng();
+                      console.log("Latitude:", lat);
+                      console.log("Longitude:", lng);
+                    }}
+                    className="w-full border border-orange-200 rounded-full p-2 text-gray-800 bg-[#faf5ed]"
+                  />
+                </div>
+              </div>
 
-          {/* Earliest Arrival */}
-          <div className="flex items-center justify-between">
-            <label className="text-gray-600">Earliest Arrival</label>
-            <a href="#" className="text-[#35653e] text-sm">
-              Schedule Later
-            </a>
-          </div>
-          <div className="mt-1">
-            <input
-              type="text"
-              value="20 Min"
-              disabled
-              className="w-full border border-orange-200 rounded-full p-2 text-gray-800 bg-[#faf5ed]"
-            />
-          </div>
+              {/* Earliest Arrival */}
+              <div className="flex items-center justify-between">
+                <label className="text-gray-600">Earliest Arrival</label>
+                <a href="#" className="text-[#35653e] text-sm">
+                  Schedule Later
+                </a>
+              </div>
+              <div className="mt-1">
+                <input
+                  type="time"
+                  // value="20 Min"
+                  // disabled
+                  className="w-full border border-orange-200 rounded-full p-2 text-gray-800 bg-[#faf5ed]"
+                />
+              </div>
+            </>
+          ) : (
+            <div className="mt-8">
+              <label className="block text-gray-600 mb-1">Pickup From</label>
+              <div>
+                <Autocomplete
+                  apiKey={process.env.NEXT_PUBLIC_GOOGLE_AUTOCOMPLETE_API_KEY}
+                  onPlaceSelected={(place) => {
+                    const lat = place.geometry.location.lat();
+                    const lng = place.geometry.location.lng();
+                    console.log("Latitude:", lat);
+                    console.log("Longitude:", lng);
+                  }}
+                  className="w-full border border-orange-200 rounded-full p-2 text-gray-800 bg-[#faf5ed]"
+                />
+              </div>
+            </div>
+          )}
         </div>
 
         {/* Fast & Free Delivery Image Section */}
